73be6ce8aebd4b5677207f6a981d5795f7e61b63
Nathan Coxson <nathancoxson2739@gmail.com>
2024-03-28 06:29:54 -0400

    added first shaders that draw a triangle primitive to a canvas

---
+++ index.js
@@ -0,0 +1,100 @@
+const canvas = document.querySelector("#canvas");
+
+/** @type {WebGLRenderingContext} */
+const gl = canvas.getContext("webgl");
+
+if (!gl) {
+    console.log("Failed to get WebGL context.");
+} else {
+    console.log("WebGL context successfully created.");
+}
+
+/**
+    * @param {WebGLRenderingContext} gl
+*/
+function createShader(gl, type, source) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if (success) {
+        return shader;
+    }
+
+    console.log(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+}
+
+const vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
+const fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
+
+const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+/**
+    * @param {WebGLRenderingContext} gl
+*/
+function createProgram(gl, vertexShader, fragmentShader) {
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if (success) {
+        return program;
+    }
+
+    console.log(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+}
+
+const program = createProgram(gl, vertexShader, fragmentShader);
+
+const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+const positions = [
+    0, 0,
+    0, 0.5,
+    0.7, 0,
+];
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+function resizeCanvasToDisplaySize(canvas) {
+  // Lookup the size the browser is displaying the canvas in CSS pixels.
+  const displayWidth  = canvas.clientWidth;
+  const displayHeight = canvas.clientHeight;
+ 
+  // Check if the canvas is not the same size.
+  const needResize = canvas.width  !== displayWidth ||
+                     canvas.height !== displayHeight;
+ 
+  if (needResize) {
+    // Make the canvas the same size
+    canvas.width  = displayWidth;
+    canvas.height = displayHeight;
+  }
+ 
+  return needResize;
+}
+resizeCanvasToDisplaySize(gl.canvas);
+gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+gl.clearColor(0, 0, 0, 0);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+gl.useProgram(program);
+
+gl.enableVertexAttribArray(positionAttributeLocation);
+const size = 2;
+const type = gl.FLOAT;
+const normalize = false;
+const stride = 0;
+const offset = 0;
+gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
+
+const primitiveType = gl.TRIANGLES;
+const draw_offset = 0;
+const count = 3;
+gl.drawArrays(primitiveType, draw_offset, count);
