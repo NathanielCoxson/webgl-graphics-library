6624884de7f8a397fda879958583254a5bc40988
Nathan Coxson <nathancoxson2739@gmail.com>
2024-04-08 01:48:24 -0400

    feat: added a color class for RGB color values (#4)

---
+++ lib/Color.ts
@@ -0,0 +1,15 @@
+export default class Color {
+    R: number;
+    G: number;
+    B: number;
+
+    constructor(
+        r: number,
+        g: number,
+        b: number
+    ) {
+        this.R = r;
+        this.G = g;
+        this.B = b;
+    }
+}
